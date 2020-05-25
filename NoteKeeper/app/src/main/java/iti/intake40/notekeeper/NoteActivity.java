44c429bca06418dc1f019a096abbe76f3d13c551
RemonShehata <RemonGerges94@gmail.com>
2020-05-25 20:07:23 +0200

    Handle new note creation

--- NoteKeeper/app/src/main/java/iti/intake40/notekeeper/NoteActivity.java
+++ NoteKeeper/app/src/main/java/iti/intake40/notekeeper/NoteActivity.java
@@ -17,6 +17,7 @@ public class NoteActivity extends AppCompatActivity {
 
     public static final String NOTE_INFO = "iti.intake40.notekeeper.NOTE_INFO";
     private NoteInfo mNote;
+    private boolean mIsNewNote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +29,7 @@ public class NoteActivity extends AppCompatActivity {
         Spinner spinnerCourses = findViewById(R.id.spinner_courses);
 
         List<CourseInfo> courses = DataManager.getInstance().getCourses();
-        
+
         ArrayAdapter<CourseInfo> adapterCourses =
                 new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, courses);
 
@@ -40,8 +41,9 @@ public class NoteActivity extends AppCompatActivity {
 
         EditText textNoteTitle = findViewById(R.id.text_note_title);
         EditText textNoteText = findViewById(R.id.text_note_text);
-
-        displayNotes(spinnerCourses, textNoteTitle, textNoteText);
+        if (!mIsNewNote) {
+            displayNotes(spinnerCourses, textNoteTitle, textNoteText);
+        }
     }
 
     private void displayNotes(Spinner spinnerCourses, EditText textNoteTitle, EditText textNoteText) {
@@ -78,5 +80,7 @@ public class NoteActivity extends AppCompatActivity {
     private void readDisplayStateValues() {
         Intent intent = getIntent();
         mNote = intent.getParcelableExtra(NOTE_INFO);
+
+        mIsNewNote = mNote == null;
     }
 }
