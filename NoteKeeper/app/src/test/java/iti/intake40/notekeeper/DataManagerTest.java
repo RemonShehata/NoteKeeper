e39a2ad8349d3e26e60d09827c8876947d24eb8d
RemonShehata <RemonGerges94@gmail.com>
2020-06-26 02:10:35 +0200

    Use of BeforeClass and replace dm with sDataManger

--- NoteKeeper/app/src/test/java/iti/intake40/notekeeper/DataManagerTest.java
+++ NoteKeeper/app/src/test/java/iti/intake40/notekeeper/DataManagerTest.java
@@ -2,31 +2,37 @@ package iti.intake40.notekeeper;
 
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class DataManagerTest {
 
+    static DataManager sDataManger;
+
+    @BeforeClass
+    public static void classSetUp(){
+        sDataManger = DataManager.getInstance();
+    }
+
     @Before
     public void setUp(){
-        DataManager dm = DataManager.getInstance();
-        dm.getNotes().clear();
-        dm.initializeExampleNotes();
+        sDataManger.getNotes().clear();
+        sDataManger.initializeExampleNotes();
     }
 
     @Test
     public void createNewNote() {
-        DataManager dm = DataManager.getInstance();
-        final CourseInfo course = dm.getCourse("android_async");
+        final CourseInfo course = sDataManger.getCourse("android_async");
         final String noteTitle = "Test note title";
         final String noteText = "This is a text body of my test note";
 
-        int noteIndex = dm.createNewNote();
-        NoteInfo newNote = dm.getNotes().get(noteIndex);
+        int noteIndex = sDataManger.createNewNote();
+        NoteInfo newNote = sDataManger.getNotes().get(noteIndex);
         newNote.setCourse(course);
         newNote.setTitle(noteTitle);
         newNote.setText(noteText);
 
-        NoteInfo compareNote = dm.getNotes().get(noteIndex);
+        NoteInfo compareNote = sDataManger.getNotes().get(noteIndex);
         Assert.assertEquals(course, compareNote.getCourse());
         Assert.assertEquals(noteTitle, compareNote.getTitle());
         Assert.assertEquals(noteText, compareNote.getText());
@@ -34,28 +40,27 @@ public class DataManagerTest {
 
     @Test
     public void findSimilarNotes(){
-        DataManager dm = DataManager.getInstance();
-        final CourseInfo course = dm.getCourse("android_async");
+        final CourseInfo course = sDataManger.getCourse("android_async");
         final String noteTitle = "Test note title";
         final String noteText1 = "This is a text body of my test note";
         final String noteText2 = "This is a text body of second test note";
 
-        int noteIndex1 = dm.createNewNote();
-        NoteInfo newNote1 = dm.getNotes().get(noteIndex1);
+        int noteIndex1 = sDataManger.createNewNote();
+        NoteInfo newNote1 = sDataManger.getNotes().get(noteIndex1);
         newNote1.setCourse(course);
         newNote1.setTitle(noteTitle);
         newNote1.setText(noteText1);
 
-        int noteIndex2 = dm.createNewNote();
-        NoteInfo newNote2 = dm.getNotes().get(noteIndex2);
+        int noteIndex2 = sDataManger.createNewNote();
+        NoteInfo newNote2 = sDataManger.getNotes().get(noteIndex2);
         newNote2.setCourse(course);
         newNote2.setTitle(noteTitle);
         newNote2.setText(noteText2);
 
-        int foundIndex1 = dm.findNote(newNote1);
+        int foundIndex1 = sDataManger.findNote(newNote1);
         Assert.assertEquals(noteIndex1, foundIndex1);
 
-        int foundIndex2 = dm.findNote(newNote2);
+        int foundIndex2 = sDataManger.findNote(newNote2);
         Assert.assertEquals(noteIndex2, foundIndex2);
     }
 }
