a1d189b199c27a15af5ae9fa0f5e446d278c8c33
RemonShehata <RemonGerges94@gmail.com>
2020-06-26 02:06:22 +0200

    Add unit test to test an edge case when 2 notes are similar and use of Before

--- NoteKeeper/app/src/test/java/iti/intake40/notekeeper/DataManagerTest.java
+++ NoteKeeper/app/src/test/java/iti/intake40/notekeeper/DataManagerTest.java
@@ -1,9 +1,17 @@
 package iti.intake40.notekeeper;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class DataManagerTest {
+
+    @Before
+    public void setUp(){
+        DataManager dm = DataManager.getInstance();
+        dm.getNotes().clear();
+        dm.initializeExampleNotes();
+    }
 
     @Test
     public void createNewNote() {
@@ -22,5 +30,32 @@ public class DataManagerTest {
         Assert.assertEquals(course, compareNote.getCourse());
         Assert.assertEquals(noteTitle, compareNote.getTitle());
         Assert.assertEquals(noteText, compareNote.getText());
+    }
+
+    @Test
+    public void findSimilarNotes(){
+        DataManager dm = DataManager.getInstance();
+        final CourseInfo course = dm.getCourse("android_async");
+        final String noteTitle = "Test note title";
+        final String noteText1 = "This is a text body of my test note";
+        final String noteText2 = "This is a text body of second test note";
+
+        int noteIndex1 = dm.createNewNote();
+        NoteInfo newNote1 = dm.getNotes().get(noteIndex1);
+        newNote1.setCourse(course);
+        newNote1.setTitle(noteTitle);
+        newNote1.setText(noteText1);
+
+        int noteIndex2 = dm.createNewNote();
+        NoteInfo newNote2 = dm.getNotes().get(noteIndex2);
+        newNote2.setCourse(course);
+        newNote2.setTitle(noteTitle);
+        newNote2.setText(noteText2);
+
+        int foundIndex1 = dm.findNote(newNote1);
+        Assert.assertEquals(noteIndex1, foundIndex1);
+
+        int foundIndex2 = dm.findNote(newNote2);
+        Assert.assertEquals(noteIndex2, foundIndex2);
     }
 }
