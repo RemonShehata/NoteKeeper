290ec6dc95df3ba7f7bda1a300e642ab0c3aaf4d
RemonShehata <RemonGerges94@gmail.com>
2020-07-03 15:32:53 +0200

    Created simple UITest that creates a new note

---
+++ NoteKeeper/app/src/androidTest/java/iti/intake40/notekeeper/NoteCreationTest.java
@@ -0,0 +1,33 @@
+package iti.intake40.notekeeper;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class)
+public class NoteCreationTest {
+    @Rule
+    public ActivityTestRule<NoteListActivity> mNoteListActivityRule =
+            new ActivityTestRule<>(NoteListActivity.class);
+
+    @Test
+    public void createNewNote(){
+//        ViewInteraction fabNewNote = onView(withId(R.id.fab));
+//        fabNewNote.perform(click());
+        onView(withId(R.id.fab)).perform(click());
+        onView(withId(R.id.text_note_title)).perform(typeText("Test note title"));
+        onView(withId(R.id.text_note_text))
+                .perform(typeText("This is the body of our test note"),
+                        closeSoftKeyboard());
+    }
+
+}
