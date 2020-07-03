6ee5f3dde2e5447e49173884c9924c738a187694
RemonShehata <RemonGerges94@gmail.com>
2020-07-03 21:02:05 +0200

    Create Adapter and use it with RecyclerView

--- NoteKeeper/app/src/main/java/iti/intake40/notekeeper/NoteListActivity.java
+++ NoteKeeper/app/src/main/java/iti/intake40/notekeeper/NoteListActivity.java
@@ -11,6 +11,8 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.List;
+
 public class NoteListActivity extends AppCompatActivity {
 
     //private ArrayAdapter<NoteInfo> mAdapterNotes;
@@ -61,6 +63,10 @@ public class NoteListActivity extends AppCompatActivity {
         final RecyclerView recyclerNotes = findViewById(R.id.list_notes);
         LinearLayoutManager notesLayoutManger = new LinearLayoutManager(this);
         recyclerNotes.setLayoutManager(notesLayoutManger);
+
+        List<NoteInfo> notes = DataManager.getInstance().getNotes();
+        final NoteRecyclerAdapter noteRecyclerAdapter = new NoteRecyclerAdapter(this, notes);
+        recyclerNotes.setAdapter(noteRecyclerAdapter);
     }
 
 }
